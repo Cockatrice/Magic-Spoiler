82836d7141ca21b95f7aaae1ea79777e00db6c77
tritoch <tritochs@gmail.com>
2017-06-21 09:33:13 -0500

    Update old branch value

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="deploy-to-SpoilerSeasonFiles"
+SOURCE_BRANCH="master"
 TARGET_BRANCH="files"
 
 function doCompile {
