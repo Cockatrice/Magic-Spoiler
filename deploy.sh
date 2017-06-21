3f834510207c78716a24cd1d558219a7bd389932
tritoch <tritochs@gmail.com>
2017-06-21 09:25:04 -0500

    Don't deploy non-master branches to Files branch

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     doCompile
     exit 0
