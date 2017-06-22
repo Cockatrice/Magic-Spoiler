c26b6daeba998df18d0b1bf581f0fd41974a1680
tritoch <tritochs@gmail.com>
2017-06-22 08:26:26 -0500

    Exit with error when file fails to load

--- main.py
+++ main.py
@@ -25,28 +25,28 @@ try:
 except Exception as ex:
     print "Unable to load file: set_info\nException information:\n" + str(ex.args) + \
           "\nUnable to load file: set_info"
-    sys.exit()
+    sys.exit("Unable to load file: set_info")
 try:
     with open('cards_manual') as data_file:
         manual_sets = json.load(data_file)
 except Exception as ex:
     print "Unable to load file: cards_manual\nException information:\n" + str(ex.args) + \
           "\nUnable to load file: cards_manual"
-    sys.exit()
+    sys.exit("Unable to load file: cards_manual")
 try:
     with open('cards_corrections') as data_file:
         card_corrections = commentjson.load(data_file)
 except Exception as ex:
     print "Unable to load file: cards_corrections\nException information:\n" + str(ex.args) + \
           "\nUnable to load file: cards_corrections"
-    sys.exit()
+    sys.exit("Unable to load file: cards_corrections")
 try:
     with open('cards_delete') as data_file:
         delete_cards = commentjson.load(data_file)
 except Exception as ex:
     print "Unable to load file: cards_delete\nException information:\n" + str(ex.args) + \
           "\nUnable to load file: cards_delete"
-    sys.exit()
+    sys.exit("Unable to load file: cards_delete")
 
 errorlog = []
 
