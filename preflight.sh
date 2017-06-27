90cdcde46ff7f0d621e4af286911ff95f778ebce
tritoch <tritochs@gmail.com>
2017-06-27 16:21:08 -0500

    Do not build Travis if only .md files

---
+++ preflight.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+CHANGED_FILES=`git diff --name-only HEAD HEAD~5`
+ONLY_READMES=True
+MD=".md"
+
+for CHANGED_FILE in $CHANGED_FILES; do
+  if ! [[ $CHANGED_FILE =~ $MD ]]; then
+    ONLY_READMES=False
+    break
+  fi
+done
+
+if [[ $ONLY_READMES == True ]]; then
+  echo "Only .md files found, exiting"
+  exit 1
+else
+  python verify_files.py
+fi
