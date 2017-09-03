485f93a8c1127d1345610dd8a5b8bd85cdc1fcc6
dev-id <none@example.com>
2017-09-03 09:44:17 -0500

    Also dump debug print to YAML

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     if presets['dumpErrors']:
         if errorlog != {}:
             print '//----- DUMPING ERROR LOG -----'
-            print json.dumps(errorlog, ensure_ascii=False, encoding='utf8', indent=2, sort_keys=True, separators=(',', ':'))
+            print yaml.safe_dump(errorlog, default_flow_style=False)
             print '//-----   END ERROR LOG   -----'
         else:
             print "No Detected Errors!"
