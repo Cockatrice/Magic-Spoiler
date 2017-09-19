36531f0247ae0f0084ef4eb821faca707fbf7e5e
tooomm <tooomm@users.noreply.github.com>
2017-09-19 13:44:58 +0200

    show hint for non-deploy builds too

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,6 +11,8 @@ function doCompile {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
+    # Run our compile script and let user know in logs
+    echo "Running script..."
     doCompile
     exit 0
 fi
