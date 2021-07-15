ff54e754c8fb801b4c20d54d9aca3f8eb772bc96
tooomm <tooomm@users.noreply.github.com>
2021-07-15 20:27:58 +0200

    re-add xml version and encoding information

--- magic_spoiler/__main__.py
+++ magic_spoiler/__main__.py
@@ -208,6 +208,7 @@ def open_header(card_xml_file: IO[Any]) -> None:
     :param card_xml_file: Card file path
     """
     card_xml_file.write(
+        "<?xml version='1.0' encoding='UTF-8'?>\n"
         "<cockatrice_carddatabase version='3'>\n"
         + "  <!--\n"
         + "  Created At: "
