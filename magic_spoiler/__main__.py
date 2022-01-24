ef0ced8d690f05a2101c4be1042647f5cc2ee845
tooomm <tooomm@users.noreply.github.com>
2022-01-24 15:27:22 +0100

    Update __main__.py

--- magic_spoiler/__main__.py
+++ magic_spoiler/__main__.py
@@ -644,7 +644,7 @@ def main() -> None:
     changed |= delete_old_files()
 
     # Set output to deploy
-    if os.getenv('CI'):
+    if os.environ('CI'):
         if changed:
             print("::set-output name=deploy::true")
             print("::notice title=Updates available::New spoiler files will be uploaded")
