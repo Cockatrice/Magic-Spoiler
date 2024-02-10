0efd986a167f53a4ea86c57d37e5ebeed432aa2e
tooomm <tooomm@users.noreply.github.com>
2024-02-10 15:00:13 +0100

    Allow to define a list of set_types to exclude (#303)
    * add sets
    
    * add comment
    
    * No lowercase transformation

--- magic_spoiler/__main__.py
+++ magic_spoiler/__main__.py
@@ -526,10 +526,15 @@ def get_spoiler_sets() -> List[Dict[str, str]]:
         return []
 
     spoiler_sets = []
+    # Find list of possible Set Types to exclude here: https://scryfall.com/docs/api/sets
+    excluded_set_types = ["alchemy", "masterpiece", "arsenal", "from_the_vault", "spellbook", "premium_deck", "duel_deck",
+                          "draft_innovation", "treasure_chest", "planechase", "archenemy", "vanguard", "box", "promo",
+                          "token", "memorabilia", "minigame"]
+
     for sf_set in sf_sets["data"]:
         if (
             sf_set["released_at"] >= time.strftime("%Y-%m-%d %H:%M:%S")
-            and sf_set["set_type"] != "token"
+            and sf_set["set_type"] not in excluded_set_types
             and sf_set["card_count"]
         ):
             sf_set["code"] = sf_set["code"].upper()
