6a14f8889ecd806617b6245fd9ba710e4cc5e94f
tooomm <tooomm@users.noreply.github.com>
2025-08-10 13:27:41 +0200

    fix key error

--- magic_spoiler/__main__.py
+++ magic_spoiler/__main__.py
@@ -213,12 +213,9 @@ def scryfall2mtgjson(scryfall_cards: List[Dict[str, Any]]) -> List[Dict[str, Any
     for sf_card in composed_sf_cards:
         super_types, types, sub_types = build_types(sf_card)
 
-        if "//" in sf_card.get("name", ""):
-            image = (
-                sf_card["card_faces"][0]
-                .get("image_uris", {})
-                .get("normal", "")
-            )
+        card_faces = sf_card.get("card_faces")
+        if card_faces and isinstance(card_faces, list) and len(card_faces) > 0:
+            image = card_faces[0].get("image_uris", {}).get("normal", "")
         else:
             image = sf_card.get("image_uris", {}).get("normal", "")
 
